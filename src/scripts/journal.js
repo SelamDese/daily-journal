cc945629c84312d7e4ae470c313ff784befe5b67
selam <selam0315@gmail.com>
2018-12-20 08:23:03 -0600

    fetching journal entry from api

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,34 +1,26 @@
-const journalEntries = [
-    {
-        concept: "Array methods",
-        date: "11/15/2018",
-        entry: "We learned about 4 different array methods today. forEach made sense, but the others still confuse me.",
-        mood: "Ok"
-    },
-    {
-        concept: "Objects",
-        date: "11/20/2018",
-        entry: "We learned objects today.",
-        mood: "Ok"
-    },
-]
+
 //string template that represents a single journal entry object as HTML
-// journalEntryStringTemplate=''
-// const makeJournalEntryComponent = (journalEntry)=> {
-//     let journalEntryStringTemplate=
-//     `<section>
-//         <h3>${journalEntry.concept}</h3>
-//         <p>${journalEntry.date}</p>
-//         <p>${journalEntry.entry}</p>
-//         <p>${journalEntry.mood}</p>
-//     </section>`
-//     return journalEntryStringTemplate
-// }
-// const renderJournalEntries = (Entries) => {
-//     const journalSection = document.querySelector('#journal-section')
-//     journalSection.innerHTML += journalEntries
-//     journalEntries.forEach(entry=>{
-//     let journalEntryy=makeJournalEntryComponent(entry)
-//     renderJournalEntries(journalEntryy)
-//     })
-// }
+journalEntryStringTemplate=''
+const makeJournalEntryComponent = (journalEntry)=> {
+    let journalEntryStringTemplate=
+    `<section>
+        <h3>${journalEntry.concept}</h3>
+        <p>${journalEntry.date}</p>
+        <p>${journalEntry.entry}</p>
+        <p>${journalEntry.mood}</p>
+    </section>`
+    return journalEntryStringTemplate
+}
+const renderJournalEntries = (Entries) => {
+    const journalSection = document.querySelector('#journal-section')
+    journalSection.innerHTML += Entries
+}
+fetch("http://localhost:8088/journalEntries") 
+.then(entry=>entry.json())  // Parse as JSON
+.then(entries => {
+    entries.forEach(entryy=>{
+        let journalEntryy=makeJournalEntryComponent(entryy)
+        renderJournalEntries(journalEntryy)
+        })
+        // What should happen when we finally have the array?
+    })
