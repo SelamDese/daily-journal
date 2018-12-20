4bdb825b3880bde5b2e4a1462c1f00035805a22a
selam <selam0315@gmail.com>
2018-12-20 02:20:22 -0600

    index.html modified

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,25 +1,34 @@
 const journalEntries = [
     {
-        date: "11/15/2018",
         concept: "Array methods",
+        date: "11/15/2018",
         entry: "We learned about 4 different array methods today. forEach made sense, but the others still confuse me.",
         mood: "Ok"
     },
     {
-        date: "11/20/2018",
         concept: "Objects",
+        date: "11/20/2018",
         entry: "We learned objects today.",
         mood: "Ok"
     },
 ]
 //string template that represents a single journal entry object as HTML
+journalEntryStringTemplate=''
 const makeJournalEntryComponent = (journalEntry)=> {
     let journalEntryStringTemplate=
     `<section>
-        <h3>Array methods</h3>
-        <p>07/24/2018</p>
-        <p>We learned about array methods, but only forEach made sense</p>
-        <p>ok</p>
+        <h3>${journalEntry.concept}</h3>
+        <p>${journalEntry.date}</p>
+        <p>${journalEntry.entry}</p>
+        <p>${journalEntry.mood}</p>
     </section>`
-    return 
+    return journalEntryStringTemplate
 }
+const renderJournalEntries = (journalEntryStringTemplate) => {
+    const journalSection = document.querySelector('journal-section')
+    journalSection.innerHTML += journalEntryStringTemplate
+}
+journalEntries.forEach(entry=>{
+    let journalHTML= makeJournalEntryComponent(entry)
+    renderJournalEntries(journalHTML)
+})
