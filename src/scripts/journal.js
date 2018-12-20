0b0ea5e2947018742b1d3b68ac1b7ba9c14067b8
selam <selam0315@gmail.com>
2018-12-20 00:48:21 -0600

    journal entry to the dom

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -4,5 +4,11 @@ const journalEntries = [
         concept: "Array methods",
         entry: "We learned about 4 different array methods today. forEach made sense, but the others still confuse me.",
         mood: "Ok"
-    }
+    },
+    {
+        date: "11/20/2018",
+        concept: "Objects",
+        entry: "We learned objects today.",
+        mood: "Ok"
+    },
 ]
